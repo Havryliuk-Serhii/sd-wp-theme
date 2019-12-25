d44985aa7c8f942d805f5651c9d5e09a3edcd226
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2019-12-25 17:46:08 +0200

    add new styles

--- functions.php
+++ functions.php
@@ -2,17 +2,19 @@
 
 function load_style_scripts (){
 	/*wp_enqueue_script( 'jquery-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js' );*/
-	wp_enqueue_script( 'jquery-my',  get_template_directory_uri() . '/js/jquery-1.11.3.js');
+	wp_enqueue_script( 'jquery-my',  get_template_directory_uri() . '/js/jquery-3.4.1.min.js');
 	wp_enqueue_script( 'wpbootstrap-script', get_template_directory_uri() . '/js/bootstrap.js' );
 	wp_enqueue_script( 'wpbootstrap-min-script', get_template_directory_uri() . '/js/bootstrap.min.js' );
-	wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'sd-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'bts-style', get_template_directory_uri() . '/css/bootstrap.css' );
-	wp_enqueue_style( 'style-full', get_template_directory_uri() . '/css/style.css' );
+	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/css/main.css' );
+	wp_enqueue_style( 'media-style', get_template_directory_uri() . '/css/media.css' );
+	wp_enqueue_style( 'fontawesome-style', get_template_directory_uri() . '/css/font-awesome.css' );
 }
 	
 add_action( 'wp_enqueue_scripts', 'load_style_scripts' );
-
 add_theme_support('post-thumbnails');
+
 set_post_thumbnail_size( auto, 650);
 
 	
