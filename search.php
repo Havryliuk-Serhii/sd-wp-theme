1022fb2cb9fb80d400233db84e93957a2b482cd2
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-01-24 22:36:41 +0200

    update search.php

--- search.php
+++ search.php
@@ -1,39 +1,26 @@
 <?php get_header(); ?>
-	<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
+	
     <div id="content">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12 col-sm-12 col-xs-12">
+					<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
 					<div class="wp-block inverse no-margin">
 						<div class="figure">
 							<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-							<div class="wp-block-content">
-								<div class="card-meta">					
+							<div class="post-block-content">
+								<div class="card-meta">
+									 <h2 class="post-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 								</div>
-								<h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 							</div>
 						</div>
 					</div>
+					<?php endwhile; ?>
+					<?php else: ?>
+						<p>По Вашему запросу ничего не найдено</p>				
+					<?php endif; ?>
 				</div>
 			</div>
 		</div>
-	</div>	
-<?php endwhile; ?>
-<?php else: ?>
-	<div id="content">
-		<div class="container">
-			<div class="row">
-				<p>По Вашему запросу ничего не найдено</p>
-			</div>
-		</div>
-	</div>		
-				
-<?php endif; ?>
-	<div class="container">
-		<div class="text-center">
-			<ul class="pagination">
-				<?php posts_nav_link(); ?>
-			</ul>	
-		</div>	
 	</div>
 <?php get_footer (); ?>
