543724ef92763fa7df83fabadf672463c6a311b1
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-01-24 22:23:43 +0200

    update 404.php

--- 404.php
+++ 404.php
@@ -1,13 +1,12 @@
-<?php get_header (); ?>
-	
+<?php get_header (); ?>	
+	<div class="content">
 		<div class="container">
-			<div class="text-center"><h2>Такой страницы нет <a href="<?php echo home_url(); ?>">Перейти на главную страницу</a></h2>
+			<div class="row">
+				<div class="col-md-12 col-sm-12 col-xs-12">
+					<div class="text-center"><h2>Такой страницы нет <a href="<?php echo home_url(); ?>">Перейти на главную страницу</a></h2>
+					</div>
+				</div>
 			</div>
-		</div>	
-			
-			
-		
-
-
-
+		</div>			
+	</div>
 <?php get_footer (); ?>
