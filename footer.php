16db8ca1812ab1ae6f8f842c8baa124430b5cb57
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-01-24 22:08:49 +0200

    update footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+		<div class="container">
+			<p>&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php esc_attr( bloginfo('name') ); ?>"><?php bloginfo('name'); ?></a></p>			
+		</div>
+	</footer>
+	<?php wp_footer(); ?>
+</body>
+</html>  
