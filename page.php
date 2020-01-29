c0cbbc729c189bdc9c1745a213ec755433ecb3ab
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-01-29 18:16:12 +0200

    update page.php

--- page.php
+++ page.php
@@ -1,20 +1,33 @@
 <?php get_header (); ?>
-	<?php while (have_posts()) : the_post(); ?>
-		<div class="post-block inverse">
-			<div class="figure">
-				<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
-				<div class="post-block-content">
-					<div class="card-meta">
-						<div class="post-meta-top">
-							<i class="fa fa-user" aria-hidden="true"></i> <?php the_author(); ?>
-							<span class="sep">/</span>
-						  	<i class="fa fa-calendar-o" aria-hidden="true"></i><?php the_time('j M, Y')?>		
-							<span class="sep">/</span><i class="fa fa-comment-o" aria-hidden="true"></i> <?php comments_popup_link('нет коментариев', '1 коментаний', '% коментариев')?>	
-						</div>
-					    <h2 class="post-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-				    </div>
-			    </div>	
-		    </div>
+	<?php 
+		while (have_posts()) : the_post(); ?>
+		<article class="article" id="post-<?php the_ID(); ?>">
+		    <div class="entry-content">
+    	    	<h2 class="post-title"><?php the_title(); ?></h2>
+    	    	<?php the_post_thumbnail(); ?>
+               	<?php the_content(); ?>
+    		</div>
+    	</article>
+        <div class="post-navigation">
+            <div class="row">
+              <?php the_post_navigation(); ?>  
+            </div>
+        </div>        
+        <?php if ( comments_open() || get_comments_number() ) :
+				comments_template();
+		?>
+		<?php endif; ?>  
+<?php endwhile; ?>	
+				</div>
+			</div>
 		</div>
-	<?php endwhile; ?>			
+	</div>
+	<div class="widget-area">
+		<div class="container">
+			<div class="row">
+				<?php dynamic_sidebar('footer_widget_left'); ?>
+				<?php dynamic_sidebar('footer_widget_right'); ?>
+			</div>
+		</div>
+	</div>	
 <?php get_footer (); ?>
